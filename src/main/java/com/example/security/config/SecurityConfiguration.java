77582989385e76e9cb7d15a17cceb3b13b9eecf4
Toma <tomaandrei98@gmail.com>
2023-06-24 09:11:33 +0300

    Add Swagger UI

--- src/main/java/com/example/security/config/SecurityConfiguration.java
+++ src/main/java/com/example/security/config/SecurityConfiguration.java
@@ -32,7 +32,19 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
                     auth
-                            .requestMatchers("/api/v1/auth/**")
+                            .requestMatchers(
+                                    "/api/v1/auth/**",
+                                    "/v2/api-docs",
+                                    "/v3/api-docs",
+                                    "/v3/api-docs/**",
+                                    "/swagger-resources",
+                                    "/swagger-resources/**",
+                                    "/configuration/ui",
+                                    "/configuration/security",
+                                    "/swagger-ui/**",
+                                    "/webjars/**",
+                                    "/swagger-ui.html"
+                            )
                             .permitAll()
 
                             .requestMatchers("/api/v1/management/**")
